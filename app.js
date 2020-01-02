bf0bb2e72bb5b2f26cb7cbcf32abe998a5b1c84f
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-01-01 21:00:29 -0300

    1/1

--- app.js
+++ app.js
@@ -12,17 +12,84 @@ var buttonSearchGenre = document.querySelector('#button-search-genre')
 
 var result = document.querySelector('.result')
 
-buttonInsert.onclick = insertBooks
+var buttonShowStorage = document.querySelector('.showStorage')
 
+class Book {
+    constructor(title, author, genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+}
+
+function render(books) {
+
+    for (book of books) {
+
+        var parag = document.createElement('p')
+
+        console.log(book)
+
+        parag.innerHTML += ` ${book.title} / ${book.author} / ${book.genre} <button type='button' class="btn btn-secondary ml-3 delete">  Excluir</button>  `;
+    }
+    result.appendChild(parag)
+}
+
+buttonInsert.onclick = insertBooks
+buttonShowStorage.onclick = showStorage
 
 function insertBooks() {
     let inputTitle = insertTitle.value
-    result.innerHTML = inputTitle
     let inputAuthor = insertAuthor.value
-    result.innerHTML += inputAuthor
     let inputGenre = insertGenre.value
-    result.innerHTML += inputGenre
+
+    const book = new Book(inputTitle, inputAuthor, inputGenre)
+
+    const books = getBooks()
+
+    books.push(book)
+
+    render(books)
+    saveToStorage(books)
+
+    clearFields()
+}
+
+function getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+        books = [];
+    } else {
+        books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+}
+
+function saveToStorage(books) {
+    localStorage.setItem('books', JSON.stringify(books));
+}
+
+function clearFields() {
     insertTitle.value = ''
     insertAuthor.value = ''
     insertGenre.value = ''
+}
+
+function showStorage() {
+
+    const storage = JSON.parse(localStorage.getItem('books'));
+
+    for (let i in storage) {
+
+        let element = storage[i]
+
+        let value = Object.values(element).join(' / ')
+
+        var parag = document.createElement('p')
+
+        parag.innerHTML += `${value} <button type='button' class="btn btn-secondary ml-3 delete">  Excluir</button> `
+
+        result.appendChild(parag)
+    }
 }
