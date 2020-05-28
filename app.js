699ce66599ea6b5f210983e1eac132b9e73539d4
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-05-27 22:47:40 -0300

    atualizado

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var insertTitle = document.querySelector(".insert-title");
 var insertAuthor = document.querySelector(".insert-author");
 
+var bookAlert = document.querySelector('.alert')
+var bookSend = document.querySelector('.book-send')
+
 var buttonInsert = document.querySelector("#button-insert");
 
 var searchTitle = document.querySelector(".search-title");
@@ -10,6 +13,10 @@ var buttonSearchTitle = document.querySelector("#button-search-title");
 var buttonSearchAuthor = document.querySelector("#button-search-author");
 
 const list = document.querySelector("#book-list");
+
+var controlRow = 0 //controlar o número de linhas na página
+
+var buttonShow = document.querySelector('#button-show')
 
 class Book {
     constructor(title, author) {
@@ -24,15 +31,36 @@ function displayBooks() {
     books.forEach(book => render(book));
 }
 
+//mostrar os livros cadastrados, com limite da tela 
 function render(book) {
+
+    if (controlRow < 8){ // para caber na tela apenas os oito primeiros livros
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+    
+        list.appendChild(row);
+    } else {
+        return false
+    }
+    
+    controlRow++
+}
+
+//mostrar os livros procurados no campo pesquisa
+function renderSearchBook(book) {
     const row = document.createElement("tr");
 
-    row.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-    `;
-
+        row.innerHTML = `
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+    
     list.appendChild(row);
 }
 
@@ -41,14 +69,51 @@ buttonInsert.onclick = insertBooks;
 function insertBooks() {
     let title = insertTitle.value;
     let author = insertAuthor.value;
+    let control = false
 
-    const book = new Book(title, author);
+    const books = getBooks()
 
-    render(book);
+    if(books == ''){ //se for a primeira inserção
+        const book = new Book(title, author);
 
-    saveToStorage(book);
+        render(book);
 
-    clearFields();
+        saveToStorage(book);
+
+        bookAlert.style.display = 'block'
+        bookAlert.style.background = 'blue'
+        bookAlert.innerHTML= "Livro cadastrado com sucesso"
+
+        clearFields();
+    } else {
+        //laço para verificar se já existe o livro cadastrado
+        for (i = 0; i < books.length; i++){
+            if(books[i].title.indexOf(title) != -1){ //significa que tem outro titulo igual
+                control = true
+            }             
+        }
+        if(control == true){
+            bookAlert.style.display = 'block'
+            bookAlert.style.background = 'red'
+            bookAlert.innerHTML= "Livro já cadastrado"
+            clearFields();
+        } else {
+            bookAlert.style.display = 'block'
+            bookAlert.style.background = 'blue'
+            bookAlert.innerHTML= "Livro cadastrado com sucesso"
+
+            const book = new Book(title, author);
+            render(book);
+            saveToStorage(book);
+            clearFields();
+        }
+    }       
+}
+
+//limpar o alerta quando digitar novo livro
+insertTitle.addEventListener('focus', cleanAlert, false)
+function cleanAlert(){
+    bookAlert.style.display = 'none'
 }
 
 function getBooks() {
@@ -111,14 +176,22 @@ buttonSearchTitle.onclick = searchForTitle;
 buttonSearchAuthor.onclick = searchForAuthor;
 
 function searchForTitle() {
-    const title = searchTitle.value;
+    var title = searchTitle.value;
     const books = getBooks();
+    var titleLower = ''
+
+    title = title.toLowerCase().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); }) //palavra com a inicial maiúscula
+    titleLower = title.toLowerCase() //palavra em letras minúsculas
 
     list.innerHTML = "";
 
     books.forEach(book => {
-        if (book.title === title) {
-            render(book);
+
+        // if (book.title === title) {
+        //     render(book);
+        // }
+        if(book.title.indexOf(title) != -1 || book.title.indexOf(titleLower) != - 1){
+            renderSearchBook(book);
         }
     });
 
@@ -126,18 +199,49 @@ function searchForTitle() {
 }
 
 function searchForAuthor() {
-    const author = searchAuthor.value;
+    var author = searchAuthor.value;
     const books = getBooks();
 
     list.innerHTML = "";
 
+    author = author.toLowerCase().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); })
+    authorLower = author.toLowerCase()
+
     books.forEach(book => {
-        if (book.author === author) {
-            render(book);
+        
+        // if (book.author === author) {
+        //     render(book);
+        // }
+        if (book.author.indexOf(author) != -1 || book.title.indexOf(authorLower) != - 1){ //para caso a pesquisa seja com um dos nomes
+            renderSearchBook(book);
         }
     });
 
     clearFields();
+}
+
+buttonShow.addEventListener('click', showLibrary, false)
+//mostra todos os 8 primeiros livros cadastrados
+function showLibrary(){
+    const books = getBooks()
+    let row = 0
+    list.innerHTML = "";
+
+    books.forEach(book => {
+        if(row < 8){
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+            `;
+        
+            list.appendChild(row);
+        }
+        row++
+    })
+
 }
 
 document.addEventListener("DOMContentLoaded", displayBooks);
